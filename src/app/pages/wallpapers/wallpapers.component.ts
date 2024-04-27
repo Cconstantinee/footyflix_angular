a02bd18397b981c5dc44202599914a43b4f2e83d
Cconstantinee <141138288+Cconstantinee@users.noreply.github.com>
2024-04-27 21:43:11 +0100

    front page changes

--- src/app/pages/wallpapers/wallpapers.component.ts
+++ src/app/pages/wallpapers/wallpapers.component.ts
@@ -7,18 +7,31 @@ import { Component,Input } from '@angular/core';
 })
 export class WallpapersComponent {
   @Input() title:string='default';
-  display_image:string='';
+  display_image:string='../../../assets/stadium_lights.png';
+
   setImage(){
     switch (this.title) {
       case 'front_title':
         this.display_image='../../../assets/stadium_lights.png';
         break;
-      case 'front_title':
-        
+      case 'teams_console_title':
+        this.display_image='../../../assets/captain-background.png';
         break;
-      case 'front_title':
-        
+      case 'match_maker_console':
+        this.display_image='../../../assets/match-background.png';
         break;
+        case 'admin_console':
+          this.display_image='../../../assets/admin-transformed(1).png';
+          break;
+        case 'login_console':
+          this.display_image='../../../assets/match-background.png';
+          break;
+        case 'tournament_console':
+          this.display_image='../../../assets/match-background.png';
+          break;
+        case 'profile_console':
+          this.display_image='../../../assets/match-background.png';
+          break;
       default:
         break;
     }
